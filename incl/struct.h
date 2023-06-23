cd919eed076d4f34e5c2bea2c1f480a0a7a24c47
Romain MOREL <rmorel@paul-f5Ar3s1.clusters.42paris.fr>
2023-06-23 19:12:10 +0200

    Transforming Message struct to class, parser.cpp needs to be modified and message class to be terminated

--- incl/struct.h
+++ incl/struct.h
@@ -1,21 +1,4 @@
 #ifndef STRUCT_H
 # define STRUCT_H
 
-# include "constant.h"
-# include <string>
-# include <map>
-# include <vector>
-
-typedef struct s_client
-{
-}	t_client;
-
-typedef struct s_message 
-{
-	std::map<std::string, std::string> 	tags;
-	std::string 						source;
-	t_cmd_type 							cmd;
-	std::vector<std::string> 			parameters;
-}	t_message;
-
 #endif
